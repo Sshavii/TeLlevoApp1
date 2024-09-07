1a2828ae7fe7ad07a5a8f89678f49e68eef5a09e
yovngmamba8 <163868747+yovngmamba8@users.noreply.github.com>
2024-09-07 00:12:56 -0400

    Home page
    -Footer con pefil e inicio
    -Items card con diferentes opciones de direccion
    -TypeScript configurado para que el footer desaparezca al deslizar hacia abajo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
+
+  hideFooter: boolean = false;
+  lastScrollTop: number = 0;
+  
+  onScroll(event:any){
+    const scrollTop = event.detail.scrollTop;
+
+    if(scrollTop > this.lastScrollTop){
+      //Desliza hacia abajo
+      this.hideFooter = true;
+    } else{
+      //Desliza hacia arriba
+      this.hideFooter = false;
+    }
+    this.lastScrollTop = scrollTop;
+
+
+  }
 
 }
