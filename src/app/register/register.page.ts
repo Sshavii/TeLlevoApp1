dad70723037f4fd5abaee7a5c155d7fc680e5641
Sshavii <134518111+Sshavii@users.noreply.github.com>
2024-10-20 17:04:40 -0300

    register page

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    }, { validator: this.passwordMatchValidator });
   }
 
+  // Validador para asegurar que las contraseñas coinciden
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value 
+      ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario de registro válido', this.registerForm.value);
+      // Lógica para el registro (por ejemplo, enviar datos al servidor)
+    } else {
+      console.log('Formulario de registro inválido');
+    }
+  }
 }
