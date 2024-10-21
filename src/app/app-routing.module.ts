d0d01ceb2b2ed07452279041c9a0d98e62a67ab6
Sshavii <134518111+Sshavii@users.noreply.github.com>
2024-10-21 17:19:00 -0300

    error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'error',
+    loadChildren: () => import('./error/error.module').then( m => m.ErrorPageModule)
+  },
 
 
 ];
